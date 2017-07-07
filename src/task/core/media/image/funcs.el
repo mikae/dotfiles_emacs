25b8d829e3d112f2413e493ef5392ad3349bd3e8
Yui Minae <yui@yui-pc.yui-domain>
2017-07-07 13:38:47 +0300

    Changed keymap in image-mode.

--- src/task/core/media/image/funcs.el
+++ src/task/core/media/image/funcs.el
@@ -2,32 +2,49 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Functions
-(defun serika/image/keymap ()
+;; Public
+(defun serika/image/scroll-up (&optional count)
+  "Scroll right."
+  (interactive "p")
+  (scroll-up 1))
+
+(defun serika/image/scroll-right (&optional count)
+  "Scroll right."
+  (interactive "p")
+  (scroll-right 1))
+
+(defun serika/image/scroll-bottom (&optional count)
+  "Scroll right."
+  (interactive "p")
+  (scroll-bottom 1))
+
+(defun serika/image/scroll-left (&optional count)
+  "Scroll right."
+  (interactive "p")
+  (scroll-left 1))
+
+;; Global
+(defun serika/image//auto-mode-alist ()
+  "Configure `auto-mode-alist' for image files.")
+
+(defun serika/image//keymap ()
   "Configure `image-mode-map'."
-  (setq image-mode-map (make-sparse-keymap))
+  (setq image-mode-map (let ((map (make-sparse-keymap)))
+                         (define-key image-mode-map (kbd "q")     #'serika/buffer/kill-current)
 
-  (define-key image-mode-map (kbd "q")   'serika/buffer/kill-current)
+                         (define-key image-mode-map (kbd "h")     #'serika/image/scroll-left)
+                         (define-key image-mode-map (kbd "j")     #'serika/image/scroll-down)
+                         (define-key image-mode-map (kbd "k")     #'serika/image/scroll-up)
+                         (define-key image-mode-map (kbd "l")     #'serika/image/scroll-right)
 
-  (define-key image-mode-map (kbd "h")   (lambda () (interactive) (scroll-right 1)))
-  (define-key image-mode-map (kbd "j")   (lambda () (interactive) (scroll-up 1)))
-  (define-key image-mode-map (kbd "k")   (lambda () (interactive) (scroll-down 1)))
-  (define-key image-mode-map (kbd "l")   (lambda () (interactive) (scroll-left 1)))
+                         (define-key image-mode-map (kbd "A-J")   #'image-next-file)
+                         (define-key image-mode-map (kbd "A-K")   #'image-previous-file)
 
-  (define-key image-mode-map (kbd "A-h") (lambda () (interactive) (scroll-left 10)))
-  (define-key image-mode-map (kbd "A-j") (lambda () (interactive) (scroll-up 10)))
-  (define-key image-mode-map (kbd "A-k") (lambda () (interactive) (scroll-down 10)))
-  (define-key image-mode-map (kbd "A-l") (lambda () (interactive) (scroll-right 10)))
-
-  (define-key image-mode-map (kbd "H")   'image-previous-file)
-  (define-key image-mode-map (kbd "J")   'image-next-file)
-  (define-key image-mode-map (kbd "K")   'image-previous-file)
-  (define-key image-mode-map (kbd "L")   'image-next-file)
-
-  (define-key image-mode-map (kbd "RET") (lambda ()
-                                           (interactive)
-                                           (run-associated-program buffer-file-name))))
+                         (define-key image-mode-map (kbd "C-c p") (lambda ()
+                                                                    (interactive)
+                                                                    (run-associated-program buffer-file-name))))))
 
 (defun init ()
   "Configure `image-mode'."
-  (serika/image/keymap))
+  (serika/image//auto-mode-alist)
+  (serika/image//keymap))
