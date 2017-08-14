56a48366b0d5563dc283352095c479fef1a732b3
Yui <yui@yui-pc.yui-domain>
2017-08-14 20:27:31 +0300

    Added fb2-mode plugin.

---
+++ src/task/core/media/fb2/funcs.el
@@ -0,0 +1,18 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+(defun init ()
+  "Configure fb2"
+  (serika-c/eg/add-install :type 'git
+                           :name 'fb2-mode
+                           :src  "https://github.com/mikae/fb2-mode")
+
+  (serika-c/eg/add-many 'fb2-mode
+                        ("require")
+                        (func/func/requirer fb2-mode)
+
+                        ("settings")
+                        (lambda ()
+                          (add-to-list 'auto-mode-alist '("\\.fb2\\'" . fb2-mode))))
+  )
