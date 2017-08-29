fe7d786bfade473c4fccf2aff7b7354c0564befd
Yui <yui@yui-pc.yui-domain>
2017-08-29 17:43:33 +0300

    Changed.

--- src/task/base/extension/run-assoc/funcs.el
+++ src/task/base/extension/run-assoc/funcs.el
@@ -9,12 +9,15 @@
                            :name 'run-assoc
                            :src  "https://raw.githubusercontent.com/mikae/emacswiki.org/master/run-assoc.el")
   (serika-c/eg/add-many-by-name 'run-assoc
-                        ("require")
-                        (lambda ()
-                          (require 'run-assoc))
+                                ("require")
+                                (lambda ()
+                                  (require 'run-assoc))
 
-                        ("settings")
-                        (lambda ()
-                          (setq associated-program-alist
-                                '(("animate" "\\.gif$")
-                                  ("smplayer" "\\.webm$"))))))
+                                ("settings")
+                                (lambda ()
+                                  (setq associated-program-alist
+                                        '(("animate" "\\.gif$")
+                                          ("smplayer" "\\.webm$")
+                                          ((lambda (file)
+                                             (browse-url (concat "file:///"
+                                                                 (expand-file-name file)))) "\\.html?$"))))))
