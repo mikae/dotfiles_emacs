315d5c70efced0bf55ee67c7e5c025bb5c31553d
Minae Yui <minae.yui.sain@gmail.com>
2017-11-20 16:51:05 +0300

    Last changes.

--- src/local-pre.el
+++ src/local-pre.el
@@ -2,4 +2,4 @@
 (setq user-full-name    "Minae Yui")
 (setq user-mail-address "minae.yui.sain@gmail.com")
 (setq serika-configuration-path-registration
-      (list 'user-project-directory "/home/data/Data/Projects"))
+      (list 'user-project-directory "/home/data2/Data/Projects"))
